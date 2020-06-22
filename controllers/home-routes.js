fbe0e1017ccc12542a78181444beb5db5cd831be
ktrnthsnr <asakthisner@hotmail.com>
2020-06-22 00:48:32 -0700

    added session controls

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -10,21 +10,23 @@ const { Post, User, Comment } = require('../models');
     //   res.render('homepage');
     // });
 
-// router.get('/', (req, res) => {
-//   res.render('homepage', {
-//     id: 1,
-//     post_url: 'https://handlebarsjs.com/guide/',
-//     title: 'Handlebars Docs',
-//     created_at: new Date(),
-//     vote_count: 10,
-//     comments: [{}, {}],
-//     user: {
-//       username: 'test_user'
-//     }
-//   });
-// });
+// -- test
+    // router.get('/', (req, res) => {
+    //   res.render('homepage', {
+    //     id: 1,
+    //     post_url: 'https://handlebarsjs.com/guide/',
+    //     title: 'Handlebars Docs',
+    //     created_at: new Date(),
+    //     vote_count: 10,
+    //     comments: [{}, {}],
+    //     user: {
+    //       username: 'test_user'
+    //     }
+    //   });
+    // });
 
-// populate the template with sequelize data
+// -------------------- populate the template with sequelize data 
+// -- GET ttp://localhost:3001/post ( validation URL within Insomnia )
 router.get('/', (req, res) => {
     console.log(req.session);
 Post.findAll({
@@ -56,7 +58,8 @@ Post.findAll({
       console.log(dbPostData[0]);
       // res.render('homepage', dbPostData[0]);
       // // res.render('homepage', dbPostData[0].get({ plain: true }));
-      res.render('homepage', { posts });
+      res.render('homepage', { posts, 
+        loggedIn: req.session.loggedIn }); // added for login
     })
     .catch(err => {
       console.log(err);
@@ -64,7 +67,8 @@ Post.findAll({
     });
 });
 
-// login
+// ----------------------------  login 
+//test  GET post http://localhost:3001/login
 router.get('/login', (req, res) => {
     if (req.session.loggedIn) {
       res.redirect('/');
@@ -74,47 +78,55 @@ router.get('/login', (req, res) => {
     res.render('login');
   });
 
-// -- route to a single post
+// -------------------------- route to a single post
+// //test  GET post http://localhost:3001/post/1
 router.get('/post/:id', (req, res) => {
-  Post.findOne({
-    where: {
-      id: req.params.id
-    },
-    attributes: [
-      'id',
-      'post_url',
-      'title',
-      'created_at',
-      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-    ],
-    include: [
-      {
-        model: Comment,
-        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-        include: {
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
           model: User,
           attributes: ['username']
         }
-      },
-      {
-        model: User,
-        attributes: ['username']
-      }
-    ]
-  })
-    .then(dbPostData => {
-      if (!dbPostData) {
-        res.status(404).json({ message: 'No post found with this id' });
-        return;
-      }
-      const post = dbPostData.get({ plain: true });
-      res.render('single-post', { post });
+      ]
     })
-    .catch(err => {
-      console.log(err);
-      res.status(500).json(err);
-    });
-});
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+  
+        // serialize the data
+        const post = dbPostData.get({ plain: true });
+  
+        // pass data to template
+        res.render('single-post', { 
+          post,
+          loggedIn: req.session.loggedIn    // -- only show comments form when logged in
+        });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
 
   // // -- test for route to a single post 
