a97617a8ca18727c0a8ff7756eb3d3dc81c7bd8b
ktrnthsnr <asakthisner@hotmail.com>
2020-06-22 18:23:33 -0700

    added posts to user dashboard

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -2,10 +2,51 @@ const router = require('express').Router();
 const sequelize = require('../config/connection');
 const { Post, User, Comment } = require('../models');
 
-
+// -- initial test
 // GET http://localhost:3002/dashboard
+// router.get('/', (req, res) => {
+//     res.render('dashboard', { loggedIn: true });
+//   });
+
+
+//  find add posts from a user GET http://localhost:3002/dashboard/
 router.get('/', (req, res) => {
-    res.render('dashboard', { loggedIn: true });
+    Post.findAll({
+      where: {
+          // session
+        user_id: req.session.user_id
+      },
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at',
+        [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        // include the serialize data before passing to template
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        res.render('dashboard', { posts, loggedIn: true });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
   });
 
-module.exports = router;
+  module.exports = router;
